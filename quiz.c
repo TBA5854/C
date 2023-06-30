f6f909aea0449d4a28fbb9530a5fbd39ff3b05db
TBA5854 <adhavan5030@gmail.com>
2023-06-30 16:54:14 +0530

    quiz incomplete

---
+++ quiz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	printf("Welcome to the Quiz !!!\n\n");
+	printf("There will be 1 mark for correct answer and no marks for wrong answer");
+	printf("Type \"start\" to begin");
+	char st[6];
+	scanf(" %s",st);
+	printf("Here comes the quiz")	
+	Which planet is known as the "Red Planet"?
+	What is the chemical symbol for gold?
+	Who painted the famous artwork "Mona Lisa"?
+	Which country is home to the kangaroo?
+	What is the capital city of France?
+	Who wrote the play "Romeo and Juliet"?
+	Which is the largest ocean on Earth?
+	What is the symbol for the chemical element oxygen?
+	Who is the author of the Harry Potter book series?
+	What is the capital city of Japan?
+}
