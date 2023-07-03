b52fbdd954d6386ea32cbd02e4af767c41d51ff3
TBA5854 <adhavan5030@gmail.com>
2023-07-04 01:38:36 +0530

    Rearrange done

--- Hello World.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h> //header
-//main func
-int main()
-{
-	printf("Hello World");
-}
--- practice_day_5_q_2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    for (int i = 65; i < 91; i++)
-    {
-        printf("%c , ",i);        
-    }
-    
-}
